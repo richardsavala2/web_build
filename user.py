db5cea5a04e79ac18d754472c26d60ad20d8b582
richard savala <59234279+thrillhobaby@users.noreply.github.com>
2020-11-15 16:57:37 -0700

    Add files via upload
    in user.py, added @classmethod get_by email():, get_by_id():, and register():. in user.py, added @staticmethod login_valid():

--- user.py
+++ user.py
@@ -1,19 +1,47 @@
+import uuid
+from src.common.database import Database
+
+
 class User(object):
-    def __init__(self):
-        pass
+    def __init__(self, email, password, _id=None):
+        self.email = email
+        self.password = password
+        self._id = uuid.uuid4().hex if _id is None else _id
 
-    def get_by_email(self):
-        pass
+    @classmethod
+    def get_by_email(cls, email):
+        data = Database.find_one('users', {'email': email})
+        if data is not None:
+            return cls(**data)
 
-    def get_by_id(self):
-        pass
+    @classmethod
+    def get_by_id(cls, _id):
+        data = Database.find_one('users', {'email': id})
+        if data is not None:
+            return cls(**data)
 
-    def login_valid(self):
+
+    @staticmethod
+    def login_valid(email, password):
+        # user.login_valid('user_email@gmail.com', 'password1234')
         # check if email matches password
-        pass
+        user = User.get_by_email(email)
+        if user is not None:
+            # check password
+            return user.password == password
+        return False
 
-    def register(self):
-        pass
+    @classmethod
+    def register(cls, email, password):
+        user = User.get_by_email(email)
+        if user is None:
+            # user doesn't exist, create it
+            new_user = User(email, password)
+            new_user.save_to_mongo()
+            return True
+        else:
+            # user exists :0
+            return False
 
     def login(self):
         pass
@@ -21,5 +49,12 @@ class User(object):
     def get_blogs(self):
         pass
 
-    
+    def json(self):
+        pass
+
+    def save_to_mongo(self):
+        pass
+
+
+
 
