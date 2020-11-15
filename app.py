7c20f05c7bad3512305ea7d535de9975506de332
richard savala <59234279+thrillhobaby@users.noreply.github.com>
2020-11-14 18:49:28 -0700

    Add files via upload
    in app.py, added hello_method():

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 from flask import Flask
 
+#'__main__'
+app = Flask(__name__)
 
+#www.mysite.com/api/
+@app.route('/')
+def hello_method():
+    return 'Hello, world!'
 
+if __name__ == '__main__':
+    app.run()
